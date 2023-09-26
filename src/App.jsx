e75740271d693ed75c24d02b0e5a471486593e80
gulshan07dev <gulshanku9135@gmail.com>
2023-09-26 21:23:19 +0530

    configure: redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
- 
-function App() { 
+import { useEffect, useState } from "react" ;
+import {useDispatch} from "react-redux";
+import authService from "./appwrite/auth";
+import {Outlet} from "react-router-dom";
+import {login, logout} from "./store/authSlice";
+import {Header, Footer} from "./components"; 
 
-  return (
-    <>
-       <h1>Blog App </h1>
-    </>
-  )
+function App() { 
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentser()
+      .then((userData) => {
+        if(userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+  
+  return !loading ? (
+    <main>
+      <Header />
+      {/* <Outlet /> */}
+      <Footer />
+    </main>
+  ) : (null)
 }
 
 export default App
