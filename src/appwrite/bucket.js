88fa91c859f0f5a4decccf25432f7ec1886463f8
gulshan07dev <gulshanku9135@gmail.com>
2023-09-26 20:22:21 +0530

    separate bucket and posts service

---
+++ src/appwrite/bucket.js
@@ -0,0 +1,51 @@
+import conf from "../conf/conf";
+import { Client,  Storage, Query, ID } from "appwrite";
+
+export class Bucket {
+    client = new Client(); 
+    bucket;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId); 
+        this.bucket = new Storage(this.client)
+    }
+ 
+    async uploadFile(file) {
+        try {
+            await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+            return true
+        } catch (error) {
+            throw error;
+            return false;
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true;
+        } catch (error) {
+            throw error;
+            return false;
+        }
+    }
+
+    getFilePreview(fileId) {
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
+}
+
+const bucket = new Bucket();
+export default bucket
--- src/appwrite/posts.js
+++ src/appwrite/posts.js
@@ -91,42 +91,8 @@ export class Service {
             throw error;
             return false;
         }
-    }
-
-    // file upload service
-    async uploadFile(file) {
-        try {
-            await this.bucket.createFile(
-                conf.appwriteBucketId,
-                ID.unique(),
-                file
-            )
-            return true
-        } catch (error) {
-            throw error;
-            return false;
-        }
-    }
-
-    async deleteFile(fileId) {
-        try {
-            await this.bucket.deleteFile(
-                conf.appwriteBucketId,
-                fileId
-            )
-            return true;
-        } catch (error) {
-            throw error;
-            return false;
-        }
-    }
-
-    getFilePreview(fileId) {
-        return this.bucket.getFilePreview(
-            conf.appwriteBucketId,
-            fileId
-        )
-    }
+    } 
+   
 }
 
 
