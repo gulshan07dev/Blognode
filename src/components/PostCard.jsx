af55f2a20fbff4eb06075d9c9e3828f6c7e517ba
gulshan07dev <gulshanku9135@gmail.com>
2023-10-03 12:18:01 +0530

    fix: postCard

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,20 +1,39 @@
-import postService from "../appwrite/posts";
 import bucketService from "../appwrite/bucket";
 import { Link } from "react-router-dom";
+import { MdFavorite, MdShare } from "react-icons/md";
+import postService from "../appwrite/posts";
+import { htmlToText } from "html-to-text";
+import { format } from "date-fns";
 
 export default function PostCard({ post }) {
   return (
-    <Link to={`/post/${post.$id}`}>
-      <div className="w-full bg-gray-100 rounded-xl py-4">
-        <div className="w-full justify-center mb-4">
-          <img
-            src={bucketService.getFilePreview(post.featuredImage)}
-            alt={post.title}
-            className="rounded-xl"
-          />
+    <div className="max-w-md max-md:w-full rounded overflow-hidden shadow-lg">
+      <Link to={`/post/${post.$id}`}>
+        <img
+          className="w-full"
+          src={bucketService.getFilePreview(post.featuredImage)}
+          alt={post.title}
+        />
+        <div className="px-6 py-4">
+          <h1 className="font-bold text-xl mb-2">{post.title}</h1>
+          <p className="text-gray-700 text-base">
+            {format(new Date(post.$createdAt), "MMMM")}{" "}
+            {format(new Date(post.$createdAt), "d")}{", "}
+            {format(new Date(post.$createdAt), "yyy")}
+          </p>
+          <p className="text-gray-700 text-base mt-4 line-clamp-2 lowercase">
+            {htmlToText(post.content)}
+          </p>
         </div>
-        <h2 className="text-2xl font-bold text-gray-500">{post.title}</h2>
+      </Link>
+      <div className="p-6 border-t-[1px] border-gray-100 flex gap-3 items-center">
+        <button className="icon-button text-xl  drop-shadow-2xl text-red-500 cursor-pointer">
+          <MdFavorite />
+        </button>
+        <button className="icon-button text-xl drop-shadow-2xl text-gray-700 cursor-pointer">
+          <MdShare />
+        </button>
       </div>
-    </Link>
+    </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,43 +1,43 @@
 import { useEffect, useState } from "react";
 import postService from "../appwrite/posts";
 import { Loader, PostCard, Container } from "../components";
+import { Link } from "react-router-dom";
+import parse from "html-react-parser"
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    postService.getPosts().then((posts) => {
+    postService.getPosts([]).then((posts) => {
       if (posts) {
         setPosts(posts.documents);
       }
     });
+    setIsLoading(false);
   }, []);
 
-  return posts ? (
-    <section className="flex items-center justify-center md:py-12 pt-20 pb-10 max-md:px-4">
+  return isLoading ? (
+    <Loader />
+  ) : (
+    <section className="flex items-center justify-center md:py-12 pt-10 pb-10 max-md:px-4">
       {posts.length === 0 ? (
         <div className="w-full py-8 mt-4 text-center">
           <Container>
-            <div className="flex flex-wrap">
-              <div className="p-2 w-full">
-                <h1 className="text-2xl font-bold hover:text-gray-500">
-                  Login to read posts
-                </h1>
-              </div>
-            </div>
+            <Link to="/login" className="p-2 w-full">
+              <h1 className="text-2xl font-bold hover:text-gray-500">
+                Please Login to view post !
+              </h1>
+            </Link>
           </Container>
         </div>
       ) : (
-        <Container>
-          <div className="flex flex-wrap">
+           <div className="grid grid-cols-1 place-items-center gap-7 max-md:gap-14 pb-10">
             {posts.map((post) => (
               <PostCard key={post.$id} post={post} />
             ))}
           </div>
-        </Container>
-      )}
+       )}
     </section>
-  ) : (
-    <Loader />
   );
 }
